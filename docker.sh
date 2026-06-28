2afc0f06e5f64d43bafbc4d8bbab2a36e88317d2
pedromujica1 <miottopedro@gmail.com>
2026-06-28 16:40:37 -0300

    ultimas alteracoes

--- docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker build --no-cache -t  api-rainhas .
-#docker run -d -p 18080:18080 --name servidor-rainhas api-rainhas
-
-docker tag api-rainhas pedromujica1/api-rainhas:latest
-docker push pedromujica1/api-rainhas:latest
